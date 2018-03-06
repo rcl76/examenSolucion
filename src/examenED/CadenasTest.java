6edee3e165e97fa538ccf3ebd262f80feea1ebe9
rcl76 <raquel.cdls@gmail.com>
2018-03-06 14:01:12 +0100

    EXAMEN RESUELTO

---
+++ src/examenED/CadenasTest.java
@@ -0,0 +1,19 @@
+package examenED;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadenasTest {
+
+	@Test
+	public void testQuitarAenPrimeras2Posiciones() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSonPrimerosYultimos2CharsLosMismos() {
+		fail("Not yet implemented");
+	}
+
+}
